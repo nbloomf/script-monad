e8ce74c5c8bb7f94c915ad82ceb8c51f87f09404
nbloomf <nathan.bloomfield@automattic.com>
2018-06-23 09:34:06 -0500

    Clean up Main

--- app/Main.hs
+++ app/Main.hs
@@ -2,35 +2,3 @@ module Main where
 
 main :: IO ()
 main = return ()
-
-{-
-
-import System.IO hiding (putStrLn)
-import Control.Monad.Script.Http
-
-import Prelude hiding (putStrLn)
-
-import Control.Concurrent (newMVar)
-
-main :: IO ()
-main = do
-  lock <- newMVar ()
-  (result,_,_) <- doit lock test
-  return ()
-
-data Q a = Q a
-
-evalQ :: Q a -> IO a
-evalQ (Q a) = return a
-
-test :: Http () () () () Q ()
-test = do
-  comment "howdy"
-  wait 500000
-  putStrLn "woo"
-  httpGet "https://google.com"
-  return ()
-
-doit lock = execHttpM (basicState ()) (basicEnv lock (const show) (const show) ()) (evalIO evalQ)
--}
-
